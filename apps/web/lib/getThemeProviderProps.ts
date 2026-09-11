f91a6bd96b68d94bd03a674e24000ad1fe9d0a2f
stevecopeland81-art <stevecopeland81@gmail.com>
2026-09-11 01:49:21 -0700

    feat: default dashboard and booking pages to light mode

--- apps/web/lib/getThemeProviderProps.ts
+++ apps/web/lib/getThemeProviderProps.ts
@@ -1,9 +1,8 @@
+import { EmbedTheme } from "@calcom/features/embed/lib/constants";
 import type { ReadonlyURLSearchParams } from "next/navigation";
 import { z } from "zod";
 
-import { EmbedTheme } from "@calcom/features/embed/lib/constants";
-
-const enum ThemeSupport {
+enum ThemeSupport {
   // e.g. Login Page
   None = "none",
   // Entire App except Booking Pages
@@ -107,6 +106,7 @@ export function getThemeProviderProps({
       attribute: "class",
       nonce: props.nonce,
       enableColorScheme: false,
+      defaultTheme: "light",
       enableSystem: themeSupport !== ThemeSupport.None,
     };
   }
@@ -138,6 +138,7 @@ export function getThemeProviderProps({
     storageKey,
     nonce: props.nonce,
     enableColorScheme: false,
+    defaultTheme: "light",
     // Enables theme switching based on system preference if true
     enableSystem: themeSupport !== ThemeSupport.None,
     // next-themes doesn't listen to changes on storageKey. So we need to force a re-render when storageKey changes
